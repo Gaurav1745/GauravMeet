8857e392aac75733a117f64e414f8f0f3666ae56
Gaurav Kumar <103983412+Gaurav1745@users.noreply.github.com>
2023-07-12 13:31:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const path = require("path");
 const express = require("express");
 const colors = require("colors");
-const cors = require("cors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 connectDB();
 
@@ -14,12 +13,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors({ 
-  origin: ['https://saurabhs-meet.netlify.app', 'https://meeetsaurav.netlify.app']
-}));
+// Enable CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  next();
+});
 
 // Make request using body x-www
 app.use("/api/users", require("./routes/authRoute"));
 app.use("/api/meetings", require("./routes/meetingRoute"));
 
-app.listen(process.env.PORT, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
